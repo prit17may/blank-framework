f80d10ec7151b79c2c4f895e56c9e282d0f9d543
Pritpal Singh <prit.17may@ymail.com>
2014-07-13 11:07:07 +0530

    Create db_defines.php

---
+++ include/db_defines.php
@@ -0,0 +1,8 @@
+<?php
+define('database_type', 'mysql');
+define('server', 'localhost');
+define('username', 'root');
+define('password', '');
+define('database_name', 'db_name');
+//date_default_timezone_set('');
+date_default_timezone_set('Asia/Kolkata');
